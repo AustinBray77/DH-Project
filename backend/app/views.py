34d125624ba269e5a54f39c0119978b0a1ef0501
parkj189 <156528175+parkj189@users.noreply.github.com>
2024-01-14 10:18:39 +0900

    edited

--- backend/app/views.py
+++ backend/app/views.py
@@ -13,11 +13,36 @@ def index(request):
 #Output JSON: { role: int } + Status Code
 
 def login(request):
-    loginData = checkValidLogin(request.email, request.password)
+    info = json.loads(request)
+    loginData = checkValidLogin(info.email, info.password)
 
     if (loginData.successful):
         roleDict = {"role" : loginData.role} 
         return response(json.dumps(roleDict), status = status.HTTP_200_OK)
     else:
         return response(json.dumps({}), status = status.HTTP_400_BAD_REQUEST)
+
+# SignUp (post url: “/sign-up”)
+# Used by all users
+# Input JSON: { name: string, email: string, password: string, role: int }
+# Output JSON: { role: int } + Status Code
+        # json.loads()
+
+def signup(request):
+    info = json.loads(request)
+    signupData = addPerson(info.email, info.name, info.password, info.role)
+
+    if (signupData):
+        roleDict = {"role" : signupData.role}
+        return response(json.dumps(roleDict),status = status.HTTP_200_OK)
+    else:
+        return response(json.dumps({}), status = status.HTTP_400_BAD_REQUEST)
+    
+# Respond (post url: “/respond”)
+# Used by volunteer and finder
+# Input JSON: { id: string, update_state: int }
+# Output: Status Code 
+# Updates information about a cat, possibly removes it from the database
+
+
 
