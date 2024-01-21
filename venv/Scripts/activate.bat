e83bc0dc6a8018df48c92055209db0e19105ef27
AustinBray77 <austin.bray77@gmail.com>
2024-01-21 11:39:11 -0500

    Reconfigured virtual environment

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\qkrtl\OneDrive\문서\GitHub\DH-Project\venv
+set VIRTUAL_ENV=C:\Solutions\Personal\DH-Project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
